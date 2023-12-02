b6a33ea2529f2ea609fab1e93b933110dffb0394
steeplejack <kcgori1982@gmail.com>
2023-12-02 16:22:30 +0000

    Part 2 was an easy modification of part 1

--- 2023/day2/solution_cpp/part1.cpp
+++ 2023/day2/solution_cpp/part1.cpp
@@ -3,34 +3,7 @@
 #include <iostream>
 #include <ranges>
 
-struct Game {
-    int id{0};
-    int red{0};
-    int green{0};
-    int blue{0};
-};
-
-std::ostream& operator<<(std::ostream& os, const Game& game) {
-    os << "Game{id=" << game.id << ",red=" << game.red
-       << ",green=" << game.green << ",blue=" << game.blue
-       << "}";
-    return os;
-}
-
-Game game_result(std::string_view s, int id) {
-    Game game{.id = id};
-    auto objects = std::views::split(s, ',');
-    for (auto&& it: objects) {
-        auto colour_it = std::views::split(std::string_view(it), ' ');
-        auto begin = colour_it.begin();
-        auto count = std::stoi(std::string{std::string_view{*(++begin)}});
-        auto colour = std::string_view(*(++begin));
-        if (colour == "red") { game.red = count; }
-        if (colour == "green") { game.green = count; }
-        if (colour == "blue") { game.blue = count; }
-    }
-    return game;
-}
+#include "day2.h"
 
 int main(int argc, const char* argv[]) {
     std::filesystem::path input{};
@@ -70,6 +43,7 @@ int main(int argc, const char* argv[]) {
         }
     }
 
+    std::cout << "Part 1\n";
     std::cout << "Sum = " << sum << std::endl;
     return 0;
 }
--- 2023/day2/solution_cpp/part2.cpp
+++ 2023/day2/solution_cpp/part2.cpp
@@ -1,5 +1,47 @@
+#include <filesystem>
+#include <fstream>
 #include <iostream>
-int main() {
-    std::cout << "Part2" << std::endl;
+#include <ranges>
+
+#include "day2.h"
+
+int main(int argc, const char* argv[]) {
+    std::filesystem::path input{};
+    if (argc == 1) {
+        input = "../input.txt";
+    } else {
+        input = argv[1];
+    }
+
+    if (!std::filesystem::exists(input)) {
+        throw std::invalid_argument("Input file does not exist");
+    }
+
+    std::string line{};
+    std::ifstream file{input};
+    long power_sum{0};
+    while (std::getline(file, line)) {
+        auto spl = std::views::split(line, ':');
+        if (spl.begin() != spl.end()) {
+            auto it = spl.begin();
+            auto game_id = std::string_view{*it};
+            int id = std::stoi(std::string(game_id.substr(std::string{"Game "}.size(), 3)));
+
+            it++;
+            auto rest = std::string_view{*it};
+            auto games = std::views::split(rest, ';');
+            Game max_result{.id = id};
+            for (auto games_it = games.begin(); games_it != games.end(); ++games_it) {
+                Game game = game_result(std::string_view{*games_it}, id);
+                if (game.red > max_result.red) { max_result.red = game.red; }
+                if (game.green > max_result.green) { max_result.green = game.green; }
+                if (game.blue > max_result.blue) { max_result.blue = game.blue; }
+            }
+            power_sum += max_result.power();
+        }
+    }
+
+    std::cout << "Part 2\n";
+    std::cout << "Power Sum = " << power_sum << std::endl;
     return 0;
 }
