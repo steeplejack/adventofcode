25b22b6f38b7e3435566d018153dcb370788a81e
steeplejack <kcgori1982@gmail.com>
2023-12-02 16:21:28 +0000

    Break out common functions into separate header + src

--- 2023/day2/solution_cpp/day2.cpp
+++ 2023/day2/solution_cpp/day2.cpp
@@ -1,3 +1,29 @@
-//
-// Created by Kevin Gori on 02/12/2023.
-//
+#include <iostream>
+#include <ranges>
+#include "day2.h"
+
+int Game::power() {
+    return red * green * blue;
+}
+
+std::ostream& operator<<(std::ostream& os, const Game& game) {
+    os << "Game{id=" << game.id << ",red=" << game.red
+       << ",green=" << game.green << ",blue=" << game.blue
+       << "}";
+    return os;
+}
+
+Game game_result(std::string_view s, int id) {
+    Game game{.id = id};
+    auto objects = std::views::split(s, ',');
+    for (auto&& it: objects) {
+        auto colour_it = std::views::split(std::string_view(it), ' ');
+        auto begin = colour_it.begin();
+        auto count = std::stoi(std::string{std::string_view{*(++begin)}});
+        auto colour = std::string_view(*(++begin));
+        if (colour == "red") { game.red = count; }
+        if (colour == "green") { game.green = count; }
+        if (colour == "blue") { game.blue = count; }
+    }
+    return game;
+}
--- 2023/day2/solution_cpp/day2.h
+++ 2023/day2/solution_cpp/day2.h
@@ -1,8 +1,14 @@
-//
-// Created by Kevin Gori on 02/12/2023.
-//
+#pragma once
 
-#ifndef DAY2_H
-#define DAY2_H
+struct Game {
+    int id{0};
+    int red{0};
+    int green{0};
+    int blue{0};
 
-#endif //DAY2_H
+    int power();
+};
+
+std::ostream& operator<<(std::ostream& os, const Game& game);
+
+Game game_result(std::string_view s, int id);
