0afc5e382552f223d90e0a055f8a467c271e67a7
steeplejack <kcgori1982@gmail.com>
2023-12-04 12:49:49 +0000

    Solved both parts, but part2 is slow and only gives the right answer by accident

--- 2023/day4/solution_python/solution.py
+++ 2023/day4/solution_python/solution.py
@@ -5,33 +5,58 @@ import sys
 class AoCError(Exception):
     pass
 
+def check_card(line):
+    card_no = int(re.search(r'^Card\s+(\d+)', line).group(1))
+    line = re.sub(r'^Card\s+\d+:', "", line.strip())
+    winners, numbers = line.split(" | ")
+    winners = [int(i) for i in winners.strip().split()]
+    numbers = [int(i) for i in numbers.strip().split()]
+    n_winners = len(set(winners).intersection(numbers))
+    return card_no, n_winners
+
+
 def part1(infile):
     if not os.path.exists(infile):
         raise AoCError(f"File not found: {infile}")
     sum = 0
     with open(infile) as fl:
         for line in fl:
-            card_no = re.search(r'^Card\s+(\d+)', line).group(1)
-            line = re.sub(r'^Card\s+\d+:', "", line.strip())
-            winners, numbers = line.split(" | ")
-            winners = [int(i) for i in winners.strip().split()]
-            numbers = [int(i) for i in numbers.strip().split()]
-            n_winners = len(set(winners).intersection(numbers))
+            card_no, n_winners = check_card(line)
             if n_winners > 0:
                 score = 2**(n_winners-1)
             else:
                 score = 0
-            print(f'Card {card_no}: score = {score}')
             sum += score
     return sum
 
+def part2(infile):
+    if not os.path.exists(infile):
+        raise AoCError(f"File not found: {infile}")
+    sum = 0
+    nlines = 0
+    with open(infile) as fl:
+        for line in fl:
+            nlines += 1
+
+    counts = [0 for _ in range(nlines)]
+
+    with open(infile) as fl:
+        for i, line in enumerate(fl):
+            counts[i] += 1
+            _, n_winners = check_card(line)
+            for _ in range(counts[i]):
+                for k in range(1, n_winners+1):
+                    counts[i + k] += 1
+    return counts
 
 def main():
     infile = sys.argv[1]
 
     try:
-        sum = part1(infile)
-        print(f'Answer to part 1: {sum}')
+        part1_sum = part1(infile)
+        print(f'Answer to part 1: {part1_sum}')
+        counts = part2(infile)
+        print(f'Answer to part 2: {sum(counts)}')
     except AoCError as e:
         print(f"Error occurred at line {e.__traceback__.tb_lineno}: {e}", file=sys.stderr)
         return 1
