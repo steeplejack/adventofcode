fe9ec3480383ac636f017369c2411c70da70b840
steeplejack <kcgori1982@gmail.com>
2023-12-04 13:53:47 +0000

    Make it faster?

--- 2023/day4/solution_python/solution.py
+++ 2023/day4/solution_python/solution.py
@@ -5,6 +5,7 @@ import sys
 class AoCError(Exception):
     pass
 
+
 def check_card(line):
     line = re.sub(r'^Card\s+\d+:', "", line.strip())
     winners, numbers = line.split(" | ")
@@ -14,44 +15,39 @@ def check_card(line):
     return n_winners
 
 
-def part1(infile):
+def preprocess_file(infile):
     if not os.path.exists(infile):
         raise AoCError(f"File not found: {infile}")
-    sum = 0
     with open(infile) as fl:
-        for line in fl:
-            n_winners = check_card(line)
-            if n_winners > 0:
-                score = 2**(n_winners-1)
-            else:
-                score = 0
-            sum += score
+        return [check_card(line) for line in fl]
+
+
+def part1(winners):
+    sum = 0
+    for n_winners in winners:
+        if n_winners > 0:
+            score = 2**(n_winners-1)
+        else:
+            score = 0
+        sum += score
     return sum
 
-def part2(infile):
-    if not os.path.exists(infile):
-        raise AoCError(f"File not found: {infile}")
-    nlines = 0
-    with open(infile) as fl:
-        for line in fl:
-            nlines += 1
 
-    counts = [1 for _ in range(nlines)]
-
-    with open(infile) as fl:
-        for i, line in enumerate(fl):
-            n_winners = check_card(line)
-            for j in range(n_winners):
-                counts[i+j+1] += counts[i]
+def part2(winners):
+    counts = [1 for _ in range(len(winners))]
+    for i, n_winners in enumerate(winners):
+        for j in range(n_winners):
+            counts[i+j+1] += counts[i]
     return sum(counts)
 
 def main():
     infile = sys.argv[1]
 
     try:
-        part1_sum = part1(infile)
+        winners = preprocess_file(infile)
+        part1_sum = part1(winners)
         print(f'Answer to part 1: {part1_sum}')
-        part2_sum = part2(infile)
+        part2_sum = part2(winners)
         print(f'Answer to part 2: {part2_sum}')
     except AoCError as e:
         print(f"Error occurred at line {e.__traceback__.tb_lineno}: {e}", file=sys.stderr)
