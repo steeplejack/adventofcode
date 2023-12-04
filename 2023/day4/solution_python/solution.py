4ab5be0b18b5f689aa054c0c11d2ad727b8657b9
steeplejack <kcgori1982@gmail.com>
2023-12-04 13:02:54 +0000

    Better solution to part2

--- 2023/day4/solution_python/solution.py
+++ 2023/day4/solution_python/solution.py
@@ -6,13 +6,12 @@ class AoCError(Exception):
     pass
 
 def check_card(line):
-    card_no = int(re.search(r'^Card\s+(\d+)', line).group(1))
     line = re.sub(r'^Card\s+\d+:', "", line.strip())
     winners, numbers = line.split(" | ")
     winners = [int(i) for i in winners.strip().split()]
     numbers = [int(i) for i in numbers.strip().split()]
     n_winners = len(set(winners).intersection(numbers))
-    return card_no, n_winners
+    return n_winners
 
 
 def part1(infile):
@@ -21,7 +20,7 @@ def part1(infile):
     sum = 0
     with open(infile) as fl:
         for line in fl:
-            card_no, n_winners = check_card(line)
+            n_winners = check_card(line)
             if n_winners > 0:
                 score = 2**(n_winners-1)
             else:
@@ -32,22 +31,19 @@ def part1(infile):
 def part2(infile):
     if not os.path.exists(infile):
         raise AoCError(f"File not found: {infile}")
-    sum = 0
     nlines = 0
     with open(infile) as fl:
         for line in fl:
             nlines += 1
 
-    counts = [0 for _ in range(nlines)]
+    counts = [1 for _ in range(nlines)]
 
     with open(infile) as fl:
         for i, line in enumerate(fl):
-            counts[i] += 1
-            _, n_winners = check_card(line)
-            for _ in range(counts[i]):
-                for k in range(1, n_winners+1):
-                    counts[i + k] += 1
-    return counts
+            n_winners = check_card(line)
+            for j in range(n_winners):
+                counts[i+j+1] += counts[i]
+    return sum(counts)
 
 def main():
     infile = sys.argv[1]
@@ -55,8 +51,8 @@ def main():
     try:
         part1_sum = part1(infile)
         print(f'Answer to part 1: {part1_sum}')
-        counts = part2(infile)
-        print(f'Answer to part 2: {sum(counts)}')
+        part2_sum = part2(infile)
+        print(f'Answer to part 2: {part2_sum}')
     except AoCError as e:
         print(f"Error occurred at line {e.__traceback__.tb_lineno}: {e}", file=sys.stderr)
         return 1
